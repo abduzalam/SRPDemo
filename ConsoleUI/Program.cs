d79132fd87ad90d564514480455cc5037ac329cc
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-07 13:35:40 +0000

    complete code
    This is the complete code

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,36 +9,23 @@ internal class Program
     /// <param name="args"></param>
     private static void Main(string[] args)
     {
-        Console.WriteLine("Weclome to my application!");
+        StandardMessages.WelcomeMessage();
 
         // Ask for user information
-        Person user = new Person();
-
-        Console.Write("What is your first name: ");
-        user.FirstName = Console.ReadLine();
-
-        Console.Write("What is your last name: ");
-        user.LastName = Console.ReadLine();
+        Person user = PersonDataCapture.Capture();
 
         // Checks to be sure the first and last names are valid
 
-        if(string.IsNullOrWhiteSpace(user.FirstName))
+        bool isUserValid = PersonValidator.Validate(user);
+
+        if (isUserValid == false)
         {
-            Console.WriteLine("Yoou did not give us a valid first name!");
-            Console.ReadLine();
+            StandardMessages.EndApplication();
             return;
         }
-
-        if (string.IsNullOrWhiteSpace(user.LastName))
-        {
-            Console.WriteLine("Yoou did not give us a valid last name!");
-            Console.ReadLine();
-            return;
-        }
-
         // Create a username for the person
-        Console.WriteLine($"Your username is {user.FirstName.Substring(0,1) }{user.LastName} ");
-        Console.ReadLine();
+        AccountGenerated.CreateAccount(user);
+        StandardMessages.EndApplication();
 
     }
 }
